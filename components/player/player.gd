ca2de9681326abdef5bbd483443d32534964bf82
salamey <82010136+salamey19@users.noreply.github.com>
2024-08-15 10:53:50 -0400

    godot 4.3

---
+++ components/player/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_on_beat : bool = true
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		#dash stuff, should be able to dash on every beat
+	if Input.is_action_just_pressed("dash") and is_on_beat:
+		velocity.x = direction * SPEED
+
+	move_and_slide()
