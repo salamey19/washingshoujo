b3897fa932fcbac2a4eadfc89619ba122f4540da
salamey <82010136+salamey19@users.noreply.github.com>
2024-08-16 16:09:54 -0400

    added the ability and familiar framework

---
+++ components/player/abilities.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+
+#Handling abilities and attacks here
+
+@onready var player: CharacterBody2D = $".."
+
+@onready var ability_used_this_beat : bool = false
+@onready var charges: Node2D = $Charges
+
+const FAMILIAR = preload("res://components/player/familiar.tscn")
+@onready var familiar_pos_1 : Vector2 = Vector2(-42, -36)
+@onready var familiar_pos_2 : Vector2 = Vector2(0, -45)
+@onready var familiar_pos_3 : Vector2 = Vector2(42, -36)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player.player_on_beat.connect(on_beat_weapon)
+	player.charge_added.connect(on_charge_added)
+
+
+func on_beat_weapon() -> void:
+	ability_used_this_beat = false
+
+
+var child_temp : int = 0
+#handle adding familiars here
+func on_charge_added() -> void:
+	charges.add_child(FAMILIAR.instantiate())
+	child_temp = charges.get_child_count() - 1
+
+	#based on child, change position
+	if child_temp == 0:
+		charges.get_child(child_temp).visible = true
+	elif child_temp == 1:
+		charges.get_child(child_temp).position = familiar_pos_2
+		charges.get_child(child_temp).visible = true
+	elif child_temp == 2:
+		charges.get_child(child_temp).position = familiar_pos_3
+		charges.get_child(child_temp).visible = true
+	else:
+		print("Error: overloaded the children")
+
+
+
+#tie ability speed to bpm??
+func _unhandled_input(event: InputEvent) -> void:
+
+	if player.is_on_beat and !ability_used_this_beat:
+		if event.is_action_pressed("attack"):
+			#do basic attack bonk
+			print("basic attack")
+			ability_used_this_beat = true
+		if player.current_charges > 0:
+			if event.is_action_pressed("ability1"):
+				print("ability1")
+				player.current_charges -= 1
+
+				ability_used_this_beat = true
+			if event.is_action_pressed("ability2"):
+				print("ability2")
+				player.current_charges = 0
+				ability_used_this_beat = true
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- components/player/player.gd
+++ components/player/player.gd
@@ -1,55 +1,63 @@
 extends CharacterBody2D
 
 
-#ideas balls act as charges up to 3
-#charges can block damage, but lose charge
-#charges can be used for abilities
-
-
+#movement
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
 const DASH_SPEED = 800
+var dashed_this_beat : bool = false
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#beat stuff
 var is_on_beat : bool = true
 var beat_index : int = 0
 var current_index : int = 0
+
+@export var grace_max : float = 0.9
+@export var grace_min : float = 0.3
+
+#ideas balls act as charges up to 3
+#charges can block damage, but lose charge
+#charges can be used for abilities
+#regain a charge automatically every x beat??
+
+#Abilities
+const MAX_CHARGES = 3
+@onready var current_charges : int = 0
+
+
 
 func _ready() -> void:
 	BeatDirector.beat.connect(on_beat)
 	BeatDirector.play_song("peritune_scene_tragic")
 
 
+signal player_on_beat
 func on_beat(index) -> void:
 
 	if !(index % 3):
+		dashed_this_beat = false
 		beat_index = index
 		#print(index)
 		is_on_beat = true
+		player_on_beat.emit()
 		animation_player.play("player_on_beat")
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#print(beat_index)
-	#print(BeatDirector.get_beat_offset(beat_index))
-	if BeatDirector.get_beat_offset(beat_index) < .3:
+
+	if BeatDirector.get_beat_offset(beat_index) < grace_min:
 		is_on_beat = true
-		#print("early")
-		#print("bad")
-	elif BeatDirector.get_beat_offset(beat_index) >= .9:
+	elif BeatDirector.get_beat_offset(beat_index) >= grace_max:
 		is_on_beat = true
-		#print("delay")
 	else:
 		is_on_beat = false
-		#print("good")
 
-	if is_on_beat:
-		print("dash")
-	else:
-		print("noot")
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -67,9 +75,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 
-	#dash stuff, should be able to dash on every beat
-	if Input.is_action_just_pressed("dash") and is_on_beat:
+	#dash stuff, should be able to dash once on every beat
+	if Input.is_action_just_pressed("dash") and is_on_beat and !dashed_this_beat:
+		dashed_this_beat = true
 		print("dash")
 		velocity.x = direction * 3 * DASH_SPEED
+		add_charge()
 
 	move_and_slide()
+
+signal charge_added
+func add_charge() -> void:
+	if not ((current_charges + 1) > MAX_CHARGES):
+		current_charges += 1
+		charge_added.emit()
